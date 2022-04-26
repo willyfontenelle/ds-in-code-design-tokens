cb1fd64170c4960e58eea959a3b02e2015e67094
Willy Fontenelle <willyfontenelle@gmail.com>
2022-04-25 22:34:16 -0300

    first commit

---
+++ src/config.js
@@ -0,0 +1,109 @@
+const StyleDictionary = require('style-dictionary');
+const StyleDictionaryPackage = require('style-dictionary');
+
+function registerConfig({ current, buildPath }) {
+
+    return {
+        "source": [current.source],
+        "platforms": {
+            "web/css": {
+                "transformGroup": "css",
+                "buildPath": buildPath.css,
+                "files": [{
+                    "destination": `${current.filename}.css`,
+                    "format": "css/variables"
+                }]
+            },
+            "web/scss": {
+                "transformGroup": "scss",
+                "buildPath": buildPath.scss,
+                "files": [{
+                    "destination": `${current.filename}.scss`,
+                    "format": "scss/variables",
+                    "filter": "notIsObject"
+                },
+                {
+                    "destination": `mixins.scss`,
+                    "format": "scss/mixin",
+                    "filter": "isObject"
+                }
+            ]
+            }
+        },
+    }
+}
+
+function registerFilter() {
+    StyleDictionaryPackage.registerFilter({
+        name: 'isObject',
+        matcher: function (token) {
+            return typeof token.value === 'object';
+        }
+    });
+
+    StyleDictionaryPackage.registerFilter({
+        name: 'notIsObject',
+        matcher: function (token) {
+            return typeof token.value !== 'object';
+        }
+    });
+
+}
+
+function registerFormat(){
+    StyleDictionaryPackage.registerFormat({
+        name: 'scss/mixin',
+        formatter: function({dictionary}) {
+            let output = ''
+            dictionary.allProperties.map(prop => {
+                if(prop.attributes.category == 'switch'){
+                    output += `
+                        @if $type == switch-${prop.attributes.type} {
+                            transition-duration: ${prop.value.velocity};
+                            transition-timing-function: ${prop.value.vibe};
+                        }
+                    `
+                }
+                if(prop.attributes.category == 'spin'){
+                    output += `
+                        @if $type == spin-${prop.attributes.type} {
+                            transition-duration: ${prop.value.velocity};
+                            transition-timing-function: ${prop.value.vibe};
+                            #{$trigger} {
+                                transform: rotate(${prop.value.rotation});
+                            }
+                        }
+                    `
+                }
+                if(prop.attributes.category == 'expand' ){
+                    output += `
+                    @if $type == expand-${prop.attributes.type} {
+                        transition-duration: ${prop.value.velocity};
+                        transition-timming-function: ${prop.value.vibe};
+                        #{$trigger} {
+                            transform: scale(${prop.value.scale});
+                            }
+                        }
+                    `
+                }
+            })
+
+            // @include motion-token(switch-slow. '.actived')
+
+            return `
+                @mixin motion-token($type, $trigger){
+                    ${output}
+                }
+                
+                `;
+        }
+    })
+}
+
+
+
+module.exports = {
+    registerConfig,
+    registerFilter,
+    registerFormat
+}
---
+++ src/index.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const { getBrands } = require("./brand");
+const { buildTokens } = require("./build");
+const { registerFilter, registerFormat } = require('./config');
+
+registerFilter();
+registerFormat();
+
+// dist/scss/globals.scss
+// dist/scss/marca-a/tema-'/dark.scss
+
+getBrands().map(async function (current) {
+    const buildPath = {
+        css: path.join('dist', 'css', current.dest, path.sep),
+        scss: path.join('dist', 'scss', current.dest, path.sep),
+
+    };
+
+    await buildTokens({ current, buildPath });
+
+})
